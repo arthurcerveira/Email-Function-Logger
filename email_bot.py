1112611eb8ef4a585a3a71662444ecec8759684b
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2020-03-15 11:50:06 -0300

    Initial commit. Add class structure and gitignore

---
+++ email_bot.py
@@ -0,0 +1,17 @@
+import datatime
+import smtplib
+
+
+class email_bot(object):
+    def __init__():
+        pass
+
+    def authenticate_email(login, senha):
+        pass
+
+    def log_info(function):
+        pass
+
+    def send_email(recipient):
+        pass
+
