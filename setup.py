fd097bff3e3f132d26ae1d5e6c6a7785a1295716
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2020-05-18 16:56:02 -0300

    Modify how dependecies are installed

--- setup.py
+++ setup.py
@@ -5,9 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-def load_requirements(fname):
-    reqs = parse_requirements(fname, session="test")
-    return [str(ir.req) for ir in reqs]
+requires = ['requests==2.23.0']
 
 
 setuptools.setup(
@@ -27,6 +25,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=load_requirements("requirements.txt"),
+    install_requires=requires,
     python_requires='>=3.2',
 )
